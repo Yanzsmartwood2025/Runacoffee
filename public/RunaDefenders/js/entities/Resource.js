b8f4ebb571c7ababd80fe7d8720800f355c6375f
Yanzsmartwood2025 <yanzsmartwood@gmail.com>
2025-08-03 16:37:47 -0500

    Update Resource.js

--- public/RunaDefenders/js/entities/Resource.js
+++ public/RunaDefenders/js/entities/Resource.js
@@ -1,1 +1,28 @@
+// entities/Resource.js
+import { config } from '../config.js';
+import { globalAnimationTimer } from '../main.js';
+
+class Resource {
+    constructor(x, y, type) {
+        this.x = x; this.y = y;
+        this.size = 45;
+        this.type = type;
+        this.image = new Image(); this.image.src = type === 'grain' ? config.grainImage : config.orbImage;
+        this.life = 400;
+        this.isFlying = false;
+    }
+    update() { this.life--; }
+    draw() {
+        if (this.isFlying) return;
+        const scale = 1 + Math.sin(globalAnimationTimer * 6) * 0.08;
+        const newSize = this.size * scale;
+        const sizeDiff = (newSize - this.size) / 2;
+
+        ctx.globalAlpha = this.life < 60 ? this.life / 60 : 1;
+        ctx.drawImage(this.image, this.x - sizeDiff, this.y - sizeDiff, newSize, newSize);
+        ctx.globalAlpha = 1;
+    }
+}
+
+export { Resource };
 
