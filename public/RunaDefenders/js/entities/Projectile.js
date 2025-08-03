9a44ebc477f5e3277a76d3da2a423f0cdd307e48
Yanzsmartwood2025 <yanzsmartwood@gmail.com>
2025-08-03 16:33:55 -0500

    Update Projectile.js

--- public/RunaDefenders/js/entities/Projectile.js
+++ public/RunaDefenders/js/entities/Projectile.js
@@ -1,1 +1,15 @@
 
+// entities/Projectile.js
+import { config } from '../config.js';
+
+class Projectile {
+    constructor(x, y) {
+        this.x = x; this.y = y - 10; this.width = 20; this.height = 20;
+        this.speed = config.projectileSpeed; this.power = 20;
+        this.image = new Image(); this.image.src = config.player.projectileImage;
+    }
+    update() { this.x += this.speed; }
+    draw() { ctx.drawImage(this.image, this.x, this.y, this.width, this.height); }
+}
+
+export { Projectile };
