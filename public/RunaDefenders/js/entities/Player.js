9fd59571420298d9550da0c6cf28074f2ea91ae2
Yanzsmartwood2025 <yanzsmartwood@gmail.com>
2025-08-12 11:44:21 -0500

    Update Player.js

--- public/RunaDefenders/js/entities/Player.js
+++ public/RunaDefenders/js/entities/Player.js
@@ -1,16 +1,23 @@
 // entities/Player.js
 import { config } from '../config.js';
+import { getGameState, getShootTimer, setShootTimer, getProjectiles, getGlobalAnimationTimer, getCellSize, getIsPowerActive } from '../main.js';
+import { playSound } from '../modules/audio.js';
 import { Projectile } from './Projectile.js';
-import { gameState, shootTimer, isPowerActive, projectiles, globalAnimationTimer, playSound } from '../main.js';
 
-class Player {
-    constructor() { this.reset(); }
+const canvas = document.getElementById('game-canvas');
+const ctx = canvas.getContext('2d');
+
+export class Player {
+    constructor() {
+        this.reset();
+    }
+
     reset() {
+        const cellSize = getCellSize();
         this.width = cellSize * 0.9;
         this.height = cellSize * 0.9;
         this.x = 10;
-        this.baseY = canvas.height / 2 - this.height / 2;
-        this.y = this.baseY;
+        this.y = canvas.height / 2 - this.height / 2;
         this.speed = config.playerSpeed;
         this.image = new Image();
         this.image.src = config.player.image.idle;
@@ -18,22 +25,25 @@ class Player {
         this.attackImage.src = config.player.image.attack;
         this.isAttacking = false;
     }
+
     draw() {
-        const bobble = Math.sin(globalAnimationTimer * 4) * (cellSize * 0.02);
+        const bobble = Math.sin(getGlobalAnimationTimer() * 4) * (getCellSize() * 0.02);
         ctx.drawImage(this.isAttacking ? this.attackImage : this.image, this.x, this.y + bobble, this.width, this.height);
     }
+
     update() {
         if (this.y < 0) this.y = 0;
         if (this.y > canvas.height - this.height) this.y = canvas.height - this.height;
     }
+
     shoot() {
-        if (shootTimer > 0 || gameState !== 'playing') return;
-        projectiles.push(new Projectile(this.x + this.width, this.y + this.height / 2));
-        shootTimer = isPowerActive ? config.fastShootCooldown : config.shootCooldown;
+        if (getShootTimer() > 0 || getGameState() !== 'playing') return;
+        
+        getProjectiles().push(new Projectile(this.x + this.width, this.y + this.height / 2));
+        setShootTimer(getIsPowerActive() ? config.fastShootCooldown : config.shootCooldown);
+        
         playSound('shoot', 'C5', '16n');
         this.isAttacking = true;
         setTimeout(() => this.isAttacking = false, 100);
     }
 }
-
-export { Player };
